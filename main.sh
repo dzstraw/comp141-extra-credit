9b4c80806bc778ca4ca0e008681efc61973a13fe
David <davidzafrir@gmail.com>
2023-12-03 16:20:12 -0600

    Create shell script

---
+++ main.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mkdir output
+mv textfile.txt output
+cd output
+read textfile.txt > read.txt
+pwd > pwd.txt
+ls > ls.txt
+cp textfile.txt copy.txt
+alias todayDate="date +'%d-%m-%y'"
+todayDate > output.txt
+wc -w textfile.txt > textcount.txt
+ps | head -n 5 > process.txt
+ifconfig | head -n 5 > netstat.txt
+mount | head -n 5 > mount.txt
+touch permissions.txt
+chmod 777 permissions.txt
+TESTENV1=test
+grep -E '\b[a-zA-Z]{3,}\b' textfile.txt > regex.txt
+cd ..
+

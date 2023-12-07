65b1be20efbc9ee122aa073c408321e88e546205
David <davidzafrir@gmail.com>
2023-12-07 16:43:42 -0600

    Replace alias with function, fix shell script

--- main.sh
+++ main.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 mkdir output
-mv textfile.txt output
+cp textfile.txt output
 cd output
 cat textfile.txt > read.txt
 pwd > pwd.txt
 ls > ls.txt
 cp textfile.txt copy.txt
-alias todayDate="date +'%d-%m-%y'"
+todayDate() {
+
+  date +'%d-%m-%y'
+}	
 todayDate > output.txt
 wc -w textfile.txt > textcount.txt
 ps | head -n 5 > process.txt
